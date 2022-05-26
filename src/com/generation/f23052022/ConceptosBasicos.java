14d9502f1ecbbf5e44ddc392903256b416c7376e
Alondra Valenzuela <avalenzuela736@gmail.com>
2022-05-25 22:08:47 -0400

    first commit

---
+++ src/com/generation/f23052022/ConceptosBasicos.java
@@ -0,0 +1,145 @@
+package com.generation.f23052022;
+
+public class ConceptosBasicos {
+		
+	//main ctrl+space
+		public static void main(String[] args) {
+			
+			int iva = 20;
+			int dos= 2;
+			int tres = 3;
+			
+			// variables Primitivas
+
+			// Se define el tipo de dato al cual pertenecera la variable
+			// tipo de dato nombre_varioable = asignacion;
+			String nombre = "Generation1234''\" \n\n\n Texto con Salto de Linea";
+			//el siguiente system es lo mismo que print, console.log, document.write, etc (syso ctrl+space)
+			System.out.println(nombre);
+
+			// 16 bit (solo permite un solo caracter, entre comillas simple)
+			char letraA = 'a';
+
+			// variables numericas
+			// 32bits
+			int numeroInt = 2147483647;
+			System.out.println(numeroInt + 10);
+
+			// 8bits solo 3 ddigitos
+			byte numeroByte = 127;
+			System.out.println(numeroByte);
+
+			// 16bits 5 digitos
+			short numeroShort = 32767;
+			System.out.println(numeroShort);
+
+			// 64bits 9 digitos
+			long numeroLong = 2147483647;
+			System.out.println(numeroLong);
+
+			// float y double (aceptan decimales)
+			// FLOAT solo 7 decimales
+			float numberFloat = (float) 10.23445;
+			System.out.println(numberFloat);
+			//double 15decimales
+			double numberdouble = (double) 10.123456789;
+			System.out.println(numberdouble);
+
+			// variable booleana (true o false)
+			boolean acepta = true;// false
+
+			/***************************************/
+
+			boolean compromiso = true;
+
+			if (compromiso == true) {
+				System.out.println("Estoy comprometido");
+			} else {
+				System.out.println("Me da lo mismo");
+			}
+
+			String poblacion = "356 mts^2";
+			String velocidad = "100 m/s";
+
+			// Variables Objeto
+			System.out.println(velocidad.charAt(4));
+
+			String curso = "G1";
+
+			// los String se comparan con equals
+			if (curso.equals("g1")) {
+				System.out.println("iguales");
+			} else {
+				System.out.println("distintos");
+			}
+
+			if (curso.equalsIgnoreCase("g1")) {
+				System.out.println("iguales");
+			} else {
+				System.out.println("distintos");
+			}
+
+			if(curso.toLowerCase().equals("g1")) {
+				System.out.println("iguales");
+			}
+			
+			if(curso.equals("g1".toUpperCase())) {
+				System.out.println("iguales");
+			}
+			
+			String curso2 = "";//vacio
+			
+			String curso3 = null;     //null
+			//comparar un String con null
+			if(null == curso3 ) {}
+			
+			//Float y Double
+			//cast, es convertir un dato a otro tipo de dato
+			float altura = (float) 1.73;//1.73f;
+			
+			Float peso =  85.5f;
+			Float gravedad = Float.parseFloat("9.6");
+			
+			//PARSE: convertir String a un tipo de numero
+			Integer.parseInt("23");
+			Long.parseLong("123456787654");
+			Float.parseFloat("85.5");
+			Double.parseDouble("123245.56");
+			
+			//ejercicio
+			int num1= 12;
+			
+			/*CONVERSION DE DATOS*/
+			//automatica
+			// byte>short>int>long>float>double
+			
+			int num4= 18;
+			float num4f= num4;
+			System.out.println(num4+" "+num4f);
+			
+			//manual
+			//double>float>long>int>...
+			Double peso2 = 75.9;
+			float peso2PF = peso2.floatValue();
+			Float peso2F =  peso2PF;
+			//Float peso2F = peso2.floatValue();
+			
+			float numPrimitivo = 123.4f;
+			Float numObjeto = 123.45f;
+			
+			// de float a int, SE PIERDEN LOS DECIMALES
+			// Float a Integer, SE PIERDEN LOS DECIMALES
+			
+			
+			// + - * / % (operadores)
+			// == != > >= < <= (comparacion)
+
+			
+			if( iva == (3 + dos) ) {
+				
+			}
+		}
+			
+			
+
+}
---
+++ src/com/generation/f23052022/Ejercicios.java
@@ -0,0 +1,38 @@
+package com.generation.f23052022;
+
+public class Ejercicios {
+	public static void main(String[] args) {
+				//to print your full name
+		String fullName = "INGRESA TU NOMBRE";
+		System.out.println(fullName);
+		
+		//to print your gender
+		char gender = 'F';
+		System.out.println(gender);
+		
+		//to print your age
+		int age = 24;
+		System.out.println(age + " years");
+		
+		//to print your citizenship
+		String citizenship = "INGRESA TU CIUDADANIA";
+		System.out.println(citizenship);
+		
+		//to print your hair colour
+		String hairCol = "INGRESA ACA TU COLOR DE PELO";
+		System.out.println(hairCol);
+		
+		//to print your career or occupation
+		String occupation = "INGRESA TU OCUPACION";
+		System.out.println(occupation);
+		
+		//to print marital status using conditional IF and a boolean
+		boolean married = false;
+		if (married == true) {
+			System.out.println("Married");
+		} else {
+			System.out.println("Single");
+		}
+	}
+
+}
